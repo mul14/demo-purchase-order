bf45c4ee0580af882d60e83c8a7555d6f17e8bed
Mulia Arifandi Nasution <mul14.net@gmail.com>
2015-12-04 09:09:07 +0700

    Add Item model, migration, factory, and seeder.

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,21 +1,18 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| Here you may define all of your model factories. Model factories give
-| you a convenient way to create models for testing and seeding your
-| database. Just tell the factory how a default model should look.
-|
-*/
-
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->email,
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Item::class, function (Faker\Generator $faker) {
+    return [
+        'artno' => strtoupper(str_random(6)),
+        'description' => $faker->sentence,
+        'price' => $faker->randomNumber(6),
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
+        $this->call(ItemTableSeeder::class);
         // $this->call(UserTableSeeder::class);
 
         Model::reguard();
